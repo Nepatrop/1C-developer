e6a2dc88beaf2ab3bf834b6e292f54df96894878
Danila <103584554+Nepatrop@users.noreply.github.com>
2023-01-11 20:39:44 +0500

    Summary

--- OneCDev/main/urls.py
+++ OneCDev/main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.call)
+    path('', views.mainCall),
+    path('demand', views.demandCall),
+    path('geography', views.geographyCall),
+    path('skills', views.skillsCall),
+    path('latestVacancies', views.latestVacanciesCall)
 ]
--- OneCDev/main/views.py
+++ OneCDev/main/views.py
@@ -3,5 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def call(request):
+def mainCall(request):
     return render(request, 'main/index.html')
+
+def demandCall(request):
+    return render(request, 'main/demand.html')
+
+def geographyCall(request):
+    return render(request, 'main/geography.html')
+
+def skillsCall(request):
+    return render(request, 'main/skills.html')
+
+def latestVacanciesCall(request):
+    return render(request, 'main/latestVacancies.html')
